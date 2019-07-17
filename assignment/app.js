ed0a286f89ec59d09259319e6b9a2bd1f360719c
kelz877 <52162275+kelz877@users.noreply.github.com>
2019-07-17 17:13:14 -0500

    Wednesday Assignment Menu

--- assignment/app.js
+++ assignment/app.js
@@ -1,80 +1,76 @@
+//let menuDiv = document.getElementById("menuDiv")
+let startersDiv = document.getElementById("startersDiv")
+let entreesDiv = document.getElementById("entreesDiv")
+let dessertsDiv = document.getElementById("dessertsDiv")
 
+//let menuItemDiv = dishes.map
+//filter out the meal types
+let starters = dishes.filter(mealType => mealType.course === "Starters");
+let entrees = dishes.filter(mealType => mealType.course === "Entrees");
+let desserts = dishes.filter(mealType => mealType.course === "Desserts");
 
-//add button functionality
-//create the button var
-let addButton = document.getElementById("addButton")
+//console.log(desserts)
 
-//create the text to be added var
-let enterTaskBox = document.getElementById("enterTaskBox")
+let menuStarters = starters.map(menuItem => {
 
-//someplace to store the tasks to be done
-let pendingTasksDiv = document.getElementById("pendingTasksDiv")
+    let starterDiv = `<div class="uk-flex menuItemNew">
+                        
+                        <img class="itemPhoto  uk-visible@s uk-section-small" src="${menuItem.imageURL}"></img>
+                        <div class="itemInfo">
+                        <h4 class="itemTitle uk-flex uk-flex-between">${menuItem.title}</h4>
+                        <p class="itemDescription uk-flex uk-flex-between">${menuItem.description}</p>
+                        <span class="uk-flex uk-flex-between">$ ${menuItem.price}</span>     
+                        </div>
+                        </div>`
+        return starterDiv
+})
 
-//store the completed tasks
-let completedTasksDiv = document.getElementById("completedTasksDiv")
+let menuEntrees = entrees.map(menuItem => {
 
-//make the button save when clicked - what should be saved? 
-addButton.addEventListener('click', function(){
-    console.log("button click fired...")
-
-    let task = enterTaskBox.value
-    enterTaskBox.value = ""
-
-    //create div element
-    let taskDiv = document.createElement("div")
-
-    //create a span tag
-    let taskNameSpan = document.createElement("span")
-    taskNameSpan.innerHTML = task
-
-    //create a check box
-    let taskNameCheckBox = document.createElement("input")
-    taskNameCheckBox.setAttribute("type", "checkbox")
-    taskNameCheckBox.className = "checkBoxNew"
-    //create a remove funciton?
-    let taskNameRemove = document.createElement("button")
-    taskNameRemove.innerHTML = "Remove"
-
-    //append everything to the div
-    taskDiv.appendChild(taskNameCheckBox)    
-    taskDiv.appendChild(taskNameSpan)
-    taskDiv.appendChild(taskNameRemove)
-
-    pendingTasksDiv.appendChild(taskDiv)
-
-    // checkbox move
-    /* taskNameCheckBox.addEventListener('click', function(){
-        console.log("check box checked...")
-
-        completedTasksDiv.appendChild(taskDiv)
-
-       taskNameCheckBox.addEventListener('click', function() {
-            pendingTasksDiv.appendChild(taskDiv)
-        })
-
-    }) */
-
-    //onclick function versus using an eventlistener
-    taskNameCheckBox.onclick = function() {
-        console.log("check box checked.....")
-        if(this.checked){
-            completedTasksDiv.appendChild(taskDiv)
-        } else {
-            pendingTasksDiv.appendChild(taskDiv)
-        }
-    }
-
-    
-
-
-    //remove task
-    taskNameRemove.addEventListener('click', function(){
-        console.log("remove button has been clicked...")
-        taskDiv.remove()
-
-    })
-
-
+    let entreeDiv = `<div class="uk-flex menuItemNew ">
+                        
+                        <img class="itemPhoto uk-visible@s uk-section-small" src="${menuItem.imageURL}"></img>
+                        <div class="itemInfo">
+                        <h4 class="itemTitle uk-flex uk-flex-between">${menuItem.title}</h4>
+                        <p class="itemDescription uk-flex uk-flex-between">${menuItem.description}</p>
+                        <span class="uk-flex uk-flex-between">$ ${menuItem.price}</span>     
+                        </div>
+                        </div>`
+        return entreeDiv
 
 })
 
+let menuDesserts = desserts.map(menuItem => {
+
+    let dessertDiv = `<div class="uk-flex menuItemNew">
+                        
+                        <img class="itemPhoto uk-visible@s uk-section-small" src="${menuItem.imageURL}"></img>
+                        <div class="itemInfo">
+                        <h4 class="itemTitle uk-flex uk-flex-between">${menuItem.title}</h4>
+                        <p class="itemDescription uk-flex uk-flex-between">${menuItem.description}</p>
+                        <span class="uk-flex uk-flex-between">$ ${menuItem.price}</span>     
+                        </div>
+                        </div>`
+        return dessertDiv
+})
+
+
+
+startersDiv.innerHTML = menuStarters.join("")
+entreesDiv.innerHTML = menuEntrees.join("")
+dessertsDiv.innerHTML = menuDesserts.join("")
+
+
+
+
+/* let menuDesserts = desserts.map(menuItem => {
+
+    let dessertDiv = `<div>
+                        <img class="itemPhoto" src="${menuItem.imageURL}"></img>
+                        <h3 class="itemTitle">${menuItem.title}</h3>
+                        <p class="itemDescription">${menuItem.description}</p>
+                        <span>$ ${menuItem.price}</span>
+                    </div>`
+        return dessertDiv
+})
+*/
